de00cde284bb6dfe2d8f8ebc67f68d6e83d6a39c
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-20 19:55:00 +0300

    wrap the navigator

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack"
+import IndexScreen from "./src/screens/IndexScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+    Index : IndexScreen
+},{
+  initialRouteName : 'Index',
+  defaultNavigationOptions : {
+    title : 'Blogs'
+  }
 });
+
+const App = createAppContainer(navigator);
+
+export default createAppContainer(navigator);
