d5ae6b4ab368a390001193c4a982685ace004bf2
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-20 20:21:06 +0300

    context added

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack"
 import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/contexts/BlogContext";
 
 const navigator = createStackNavigator({
     Index : IndexScreen
@@ -11,6 +13,19 @@ const navigator = createStackNavigator({
   }
 });
 
+{/*
+  // Instead of exporting like this we first create App then export it 
+  // to wrap the navigator 
+
+  // that allows us to convert the navigator into a React component that can be rendered in the app
+
+  export default createAppContainer(navigator);
+*/}
+
 const App = createAppContainer(navigator);
 
-export default createAppContainer(navigator);
+export default () => {
+  return <BlogProvider>
+    <App/>
+  </BlogProvider>
+};
